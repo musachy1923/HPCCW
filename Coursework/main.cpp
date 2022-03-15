4875aa2e14eb126123ba389e5aea23984bef3499
musachy1923 <musa.achoudhury@gmail.com>
2022-03-15 16:30:27 +0000

    BC applied

--- Coursework/main.cpp
+++ Coursework/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+//useful macros
+#define f1(x) (epsilon*u[x]*(1-u[x])*(u[x]-(v[x]+b)/a))*dt+u[x]
+#define f2(x) (u[x]*u[x]*u[x]-v[x])*dt+v[x]
+#define neuman(x, a, b, c, d) (1/h/h*(x[a]+x[b]+x[c]-3*x[d]))*dt
+#define neuman2(x, a, b, c) (1/h/h*(x[a]+x[b]-2*x[c]))
+
 using namespace std;
 
-#define u_n_1(x) for (1/h/h*(u[x]+u[x]+u[x]-3*u[x])+epsilon*u[x]*(1-u[x])*(u[x]-(v[x]+b)/a))*dt+u[x];
-#define v_n_1(x) for (1/h/h*(v[x]+v[x]+v[x]-3*v[x])+u[x]*u[x]*u[x]-v[x)*dt+v[x];
+
+
 void printMatrix(double* A, const int& M, const int& N);
-void writeFile(string fileName, int Ny, int Nx, double* x);
+void writeFile(string fileName, int Ny, int Nx, int tn, double* x);
 
 int main(int argc, char **argv)
 {
@@ -43,7 +49,7 @@ int main(int argc, char **argv)
     
     //boundary conditions from the handout
     double dt = 0.001;
-    double t = 0.001;
+    double t = 0.003;
     int tn = t/dt; //number of time nodes
     
     double * u = new double[Nx*Ny*(tn)];
@@ -78,34 +84,40 @@ int main(int argc, char **argv)
     
     //do for i = 1, come back later for i = 0
     //eg issue with 
-for (int k = 0; k < tn; k++) {
-   ts = Nx*Ny*k;
-   t2 = Nx*Ny*(k+1)
+for (int k = 1; k < tn; k++) {
+   int ts = Nx*Ny*(k-1);
+   int t2 = Nx*Ny*(k);
         for (int i = 1; i < Ny+abs(Nx-Ny); i++) {
         if (i < Ny){
             //Left
-            u[t2+i*Nx] = (1/h/h*(u[ts+1+Nx*i]+u[ts+Nx*(i+1)]+u[ts+Nx*(i-1)]-3*u[ts+Nx*i])+epsilon*u[ts+Nx*i]*(1-u[ts+Nx*i])*(u[ts+Nx*i]-(v[ts+i*Nx]+b)/a))*dt+u[ts+Nx*i];
-            v[t2+i*Nx] = (1/h/h*(v[ts+1+Nx*i]+v[ts+Nx*(i+1)]+v[ts+Nx*(i-1)]-3*v[ts+Nx*i])+u[ts+Nx*i]*u[ts+Nx*i]*u[ts+Nx*i]-v[ts+i*Nx])*dt+v[ts+Nx*i];
+            u[t2+i*Nx] = (1/h/h*(u[ts+1+Nx*i]+u[ts+Nx*(i+1)]+u[ts+Nx*(i-1)]-3*u[ts+Nx*i]))*dt+f1(ts+Nx*i);
+            v[t2+i*Nx] = (1/h/h*(v[ts+1+Nx*i]+v[ts+Nx*(i+1)]+v[ts+Nx*(i-1)]-3*v[ts+Nx*i]))*dt+f2(ts+Nx*i);
             //Right
-            u[t2+Nx-1+i*Nx] = (1/h/h*(u[ts+Nx-2+Nx*i]+u[ts+Nx-1+Nx*(i+1)]+u[ts+Nx-1+Nx*(i-1)]-3*u[ts+Nx-1+Nx*i])+epsilon*u[ts+Nx-1+Nx*i]*(1-u[ts+Nx-1+Nx*i])*(u[ts+Nx-1+Nx*i]-(v[ts+Nx-1+Nx*i]+b)/a))*dt+u[ts+Nx-1+Nx*i];
-            v[t2+Nx-1+i*Nx] = (1/h/h*(v[ts+Nx-2+Nx*i]+v[ts+Nx-1+Nx*(i+1)]+v[ts+Nx-1+Nx*(i-1)]-3*v[ts+Nx-1+Nx*i])-3*v[ts+Nx-1+Nx*i])+u[ts+Nx-1+Nx*i]*u[ts+Nx-1+Nx*i]*u[ts+Nx-1+Nx*i]-v[ts+Nx-1+Nx*i])*dt+v[ts+Nx-1+Nx*i];
-        }
+            
+            u[t2+Nx-1+i*Nx] = (1/h/h*(u[ts+Nx-2+Nx*i]+u[ts+Nx-1+Nx*(i+1)]+u[ts+Nx-1+Nx*(i-1)]-3*u[ts+Nx-1+Nx*i]))*dt+f1(ts+Nx-1+Nx*i);
+            v[t2+Nx-1+i*Nx] = (1/h/h*(v[ts+Nx-2+Nx*i]+v[ts+Nx-1+Nx*(i+1)]+v[ts+Nx-1+Nx*(i-1)]-3*v[ts+Nx-1+Nx*i]))*dt+f2(ts+Nx-1+Nx*i);
+        
+          }
         if (i < Nx) {
             //Lower
-            u[t2+i] = (1/h/h*(u[ts+1+i]+u[ts+(i+1)]+u[ts+(i-1)]-3*u[ts+i])+epsilon*u[ts+i]*(1-u[ts+i])*(u[ts+i]-(v[ts+i]+b)/a))*dt+u[ts+i];
-            v[t2+i] = (1/h/h*(v[ts+1+i]+v[ts+(i+1)]+v[(ts+i-1)]-3*v[ts+i])+u[ts+i]*u[ts+i]*u[ts+i]-v[ts+i])*dt+v[ts+i];
+            u[t2+i] = (1/h/h*(u[ts+1+i]+u[ts+(i+1)]+u[ts+(i-1)]-3*u[ts+i]))*dt+f1(ts+i);
+            v[t2+i] = (1/h/h*(v[ts+1+i]+v[ts+(i+1)]+v[(ts+i-1)]-3*v[ts+i]))*dt+f2(ts+i);
             //Upper
-            u[t2+Nx*(Ny-1)+i] = (1/h/h*(u[ts+Nx*(Ny-2)+i]+u[ts+Nx*(Ny-1)+i+1]+u[ts+Nx*(Ny-1)+i-1]-3*u[ts+Nx*(Ny-1)+i])+epsilon*u[ts+Nx*(Ny-1)+i]*(1-u[ts+Nx*(Ny-1)+i])*(u[ts+Nx*(Ny-1)+i]-(v[ts+Nx*(Ny-1)+i]+b)/a))*dt+u[ts+Nx*(Ny-1)+i];
-            v[t2+Nx*(Ny-1)+i] = (1/h/h*(v[ts+Nx*(Ny-2)+i]+v[ts+Nx*(Ny-1)+i+1]+v[ts+Nx*(Ny-1)+i-1]-3*v[ts+Nx*(Ny-1)+i])+u[ts+Nx*(Ny-1)+i]*u[ts+Nx*(Ny-1)+i]*u[ts+Nx*(Ny-1)+i]-v[ts+Nx*(Ny-1)+i])*dt+v[ts+Nx*(Ny-1)+i];;
-        }
+            u[t2+Nx*(Ny-1)+i] = (1/h/h*(u[ts+Nx*(Ny-2)+i]+u[ts+Nx*(Ny-1)+i+1]+u[ts+Nx*(Ny-1)+i-1]-3*u[ts+Nx*(Ny-1)+i]))*dt+f1(ts+Nx*(Ny-1)+i);
+            v[t2+Nx*(Ny-1)+i] = (1/h/h*(v[ts+Nx*(Ny-2)+i]+v[ts+Nx*(Ny-1)+i+1]+v[ts+Nx*(Ny-1)+i-1]-3*v[ts+Nx*(Ny-1)+i]))*dt+f2(ts+Nx*(Ny-1)+i);
+         }
+        
 }
-u[t2] = (1/h/h*(u[ts+1]+u[ts+Nx]-2*u[ts])+epsilon*u[ts]*(1-u[ts])*(u[ts]-(v[ts]+b)/a))*dt+u[ts];
-u[t2+Nx-1] = (1/h/h*(u[ts+Nx-1]+u[ts+Nx-1]-2*u[ts+Nx-1])+epsilon*u[ts+Nx-1]*(1-u[ts+Nx-1])*(u[ts+Nx-1]-(v[ts+Nx-1]+b)/a))*dt+u[ts+Nx-1];
-u[t2+Nx*(Ny-1)] = (1/h/h*(u[ts+Nx*(Ny-1)]+u[ts+Nx*(Ny-1)]-2*u[ts+Nx*(Ny-1)])+epsilon*u[ts+Nx*(Ny-1)]*(1-u[ts+Nx*(Ny-1)])*(u[ts+Nx*(Ny-1)]-(v[ts+Nx*(Ny-1)]+b)/a))*dt+u[ts+Nx*(Ny-1)];
-u[t2+Nx*Ny] = (1/h/h*(u[ts+Nx*Ny]+u[ts+Nx*Ny]-2*u[ts+Nx*Ny])+epsilon*u[ts+Nx*Ny]*(1-u[ts+Nx*Ny])*(u[ts+Nx*Ny]-(v[ts+Nx*Ny]+b)/a))*dt+u[ts+Nx*Ny];
 
-v[t2] = (1/h/h*(v[ts]+v[ts]-2*v[ts])+u[ts]*u[ts]*u[ts]-v[ts])*dt+v[ts];
-v[t2+Nx-1] = (1/h/h*(v[ts+Nx-1]+v[ts+Nx-1]-2*v[ts+Nx-1])+u[ts+Nx-1]*u[ts+Nx-1*u[ts+Nx-1]-v[ts+Nx-1])*dt+v[ts+Nx-1];
+u[t2] = (1/h/h*(u[ts+1]+u[ts+Nx]-2*u[ts]))*dt+f1(ts);
+u[t2+Nx-1] = (1/h/h*(u[ts+Nx-1]+u[ts+Nx-1]-2*u[ts+Nx-1]))*dt+f1(ts+Nx-1);
+u[t2+Nx*(Ny-1)] = (1/h/h*(u[ts+Nx*(Ny-1)]+u[ts+Nx*(Ny-1)]-2*u[ts+Nx*(Ny-1)]))*dt+f1(ts+Nx*(Ny-1));
+u[t2+Nx*Ny] = (1/h/h*(u[ts+Nx*Ny]+u[ts+Nx*Ny]-2*u[ts+Nx*Ny]))*dt+f1(ts+Nx*Ny);
+
+v[t2] = (1/h/h*(v[ts]+v[ts]-2*v[ts]))*dt+f2(ts);
+v[t2+Nx-1] = (1/h/h*(v[ts+Nx-1]+v[ts+Nx-1]-2*v[ts+Nx-1]))*dt+f2(ts+Nx-1);
+v[t2+Nx*(Ny-1)] = (1/h/h*(v[ts+Nx*(Ny-1)]+v[ts+Nx*(Ny-1)]-2*v[ts+Nx*(Ny-1)]))*dt+f2(ts+Nx*(Ny-1));
+v[t2+Nx*Ny] = (1/h/h*(v[ts+Nx*Ny]+v[ts+Nx*Ny]-2*v[ts+Nx*Ny]))*dt+f2(ts+Nx*Ny);
 
 } 
 
@@ -120,10 +132,10 @@ v[t2+Nx-1] = (1/h/h*(v[ts+Nx-1]+v[ts+Nx-1]-2*v[ts+Nx-1])+u[ts+Nx-1]*u[ts+Nx-1*u[
     //printMatrix(u, Ny, Nx);
     //printMatrix(v, Ny, Nx);
     
-    writeFile("x.txt", Ny, Nx, x);
-    writeFile("y.txt", Ny, Nx, y);
-    writeFile("u.txt", Ny, Nx, u);
-    writeFile("v.txt", Ny, Nx, v);
+    writeFile("x.txt", Ny, Nx, tn, x);
+    writeFile("y.txt", Ny, Nx, tn, y);
+    writeFile("u.txt", Ny, Nx, tn, u);
+    writeFile("v.txt", Ny, Nx, tn, v);
     
     delete[] x;
     delete[] y;
@@ -143,14 +155,19 @@ void printMatrix(double* A, const int& M, const int& N) {
     cout << endl;
 }
 
-void writeFile(string fileName, int Ny, int Nx, double * x) {
+void writeFile(string fileName, int Ny, int Nx, int tn,  double * x) {
     ofstream file;
     file.open(fileName);
-    for (int i = 0; i < Ny; i++) {
+    for (int k = 0; k < tn; k++) {
+        for (int i = 0; i < Ny; i++) {
             for (int j = 0; j < Nx;  j++) {
-                    file << x[Nx*i+j]<<" ";
+                    
+                file << x[Nx*Ny*k+Nx*i+j]<<" ";
     }
     file << "\n";
+        
+    }
+    file << "-----------------------------------------------------------------------------------------------" << endl;
 }
     file.close();
     
